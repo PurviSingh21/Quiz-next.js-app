7e880858237efb0137b6fe71c18ddd6a1844c868
PurviSingh21 <118907108+PurviSingh21@users.noreply.github.com>
2023-11-28 21:18:45 +0530

    start

---
+++ app/data.js
@@ -0,0 +1,65 @@
+export const quiz = {
+    totalQuestions: 5,
+    questions: [
+      {
+        id: 1,
+        question: 'What is the capital of France?',
+        answers: ['Madrid', 'Paris', 'Rome', 'Berlin'],
+        correctAnswer: 'Paris',
+      },
+      {
+        id: 2,
+        question: 'What is the largest planet in our solar system?',
+        answers: ['Mars', 'Jupiter', 'Venus', 'Saturn'],
+        correctAnswer: 'Jupiter',
+      },
+      {
+        id: 3,
+        question: 'What is the smallest country in the world?',
+        answers: ['Monaco', 'Maldives', 'Vatican City', 'San Marino'],
+        correctAnswer: 'Vatican City',
+      },
+      {
+        id: 4,
+        question: 'What is the most widely spoken language in the world?',
+        answers: ['English', 'Mandarin', 'Spanish', 'Hindi'],
+        correctAnswer: 'Mandarin',
+      },
+      {
+        id: 5,
+        question: 'Who is the founder of Microsoft?',
+        answers: ['Steve Jobs', 'Bill Gates', 'Elon Musk', 'Mark Zuckerberg'],
+        correctAnswer: 'Bill Gates',
+      },
+      {
+        id: 6,
+        question: 'Where was the first example of paper money used?',
+        answers: ['China', 'Turkey', 'Greece', 'Europe'],
+        correctAnswer: 'China',
+      },
+      {
+        id: 7,
+        question: 'Which of the following languages has the longest alphabet?',
+        answers: ['Greek', 'Russian', 'Hindi', 'Arabic'],
+        correctAnswer: 'Russian',
+      },
+      {
+        id: 8,
+        question: ' The fear of insects is known as what?',
+        answers: ['Entomophobia', 'Trypophobia', 'Arachnophobia', 'Ailurophobia'],
+        correctAnswer: 'Entomophobia',
+      },
+      {
+        id: 9,
+        question: 'Which app has the most total users?',
+        answers: ['Tik Tok', 'Snapchat', 'Instagram', 'Hike'],
+        correctAnswer: 'Instagram',
+      },
+      {
+        id: 10,
+        question: 'What is the strongest muscle in the human body?',
+        answers: ['Jaw', 'Liver', 'Glutes', 'Heart'],
+        correctAnswer: 'Jaw',
+      },
+    ],
+  };
